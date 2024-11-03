fb3c5b6a8c58d5d72c9a90d6d5bc4c7ba04d6833
Hxxseung <soaorsith2@naver.com>
2024-11-03 23:43:01 +0900

    Title : Query Method란 무엇일까?

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,6 +3,9 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {
+import java.util.List;
 
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();    //해당 메서드 이름을 분석해서 SimpleJPARepository에서 구현이 됨. -> 따로 SQL 쿼리문을 구현하지 않아도 되는 장점
+    //List<Memo> findAllByUsername(String username); //username 이 작성한 모든 메모 내용을 갖고옴 -> 동적 처리
 }
