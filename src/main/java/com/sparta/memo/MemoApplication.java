34a38491c76ea36a72fb24c47b31229e98c6ca55
Hxxseung <soaorsith2@naver.com>
2024-11-03 23:29:56 +0900

    Title : JPA Auditing apply

--- src/main/java/com/sparta/memo/MemoApplication.java
+++ src/main/java/com/sparta/memo/MemoApplication.java
@@ -2,7 +2,10 @@ package com.sparta.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing  //이걸 달아줘야 Timestamped.class 코드들 적용
 @SpringBootApplication
 public class MemoApplication {
 
