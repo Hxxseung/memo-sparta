347b7de395b738f5ebd4d014b0fb1f67449b1702
Hxxseung <soaorsith2@naver.com>
2024-11-03 23:44:16 +0900

    2Week End

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();    //해당 메서드 이름을 분석해서 SimpleJPARepository에서 구현이 됨. -> 따로 SQL 쿼리문을 구현하지 않아도 되는 장점
+    List<Memo> findAllByContentsContainsOrderByModifiedAtDesc(String keyword);
     //List<Memo> findAllByUsername(String username); //username 이 작성한 모든 메모 내용을 갖고옴 -> 동적 처리
 }
+
--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,8 +4,8 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -36,10 +36,15 @@ public class MemoService {
         return memoRepository.findAllByOrderByModifiedAtDesc().stream().map(MemoResponseDto::new).toList();
     }
 
-   @Transactional
+    public List<MemoResponseDto> getMemosByKeyword(String keyword) {
+        return memoRepository.findAllByContentsContainsOrderByModifiedAtDesc(keyword).stream().map(MemoResponseDto::new).toList();
+    }
+
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = findMemo(id);
+
         // memo 내용 수정
         memo.update(requestDto);
 
@@ -50,15 +55,15 @@ public class MemoService {
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = findMemo(id);
 
-        //memo 삭제
+        // memo 삭제
         memoRepository.delete(memo);
-        return id;
 
+        return id;
     }
 
     private Memo findMemo(Long id) {
         return memoRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("선택ㄱ한 메모는 존재하지 않습니다.")
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
         );
     }
 }
